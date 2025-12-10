5d55348b1f5d47aaa705f2adb8d45116612a519b
ademkotti <adam.kotti@esprit.tn>
2025-12-10 16:25:23 +0000

    Disable JPA + SQL for Kubernetes

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -3,11 +3,18 @@ package tn.esprit.studentmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+        },
+        scanBasePackages = {"tn.empty"} // <-- un package vide pour éviter le scan
+)
 public class StudentManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementApplication.class, args);
     }
-
 }
+
+
+
